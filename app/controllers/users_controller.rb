4156b8a06c70e010f2da99ecba0cf33cd8412e62
takeo.shohei <a1921st@aiit.ac.jp>
2019-05-11 07:12:02 +0000

    Finish ch07

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,30 @@
 class UsersController < ApplicationController
+  #GET /users/:id
+  def show
+    @user = User.find(params[:id])
+    # =>  app/views/users/show.html.erb
+  end
+  
   def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save # =>  Valodation
+      # sucess
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+      # GET "/users/#{@user.id}" => show
+    else
+      # failure
+      render 'new'
+    end
+  end
+  
+  def user_params
+  params.require(:user).permit(
+    :name, :email, :password,
+    :password_confirmation)
   end
 end
