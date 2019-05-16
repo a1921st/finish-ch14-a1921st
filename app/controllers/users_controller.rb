12133545bb85e4aac2a83589f43b4f5f641f3129
takeo.shohei <a1921st@aiit.ac.jp>
2019-05-16 09:39:15 +0000

    ontheway ch10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,9 +23,29 @@ class UsersController < ApplicationController
     end
   end
   
-  def user_params
-  params.require(:user).permit(
-    :name, :email, :password,
-    :password_confirmation)
+  #GET /users/:id/edit
+  #params[:id] => :id
+  def edit
+    @user = User.find(params[:id])
+    #app/views/users/edit.html.erb
   end
+  
+  #PATCH /users/:id
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # 更新に成功した場合を扱う。
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+  private
+    def user_params
+      params.require(:user).permit(
+      :name, :email, :password,
+      :password_confirmation)
+    end
 end
