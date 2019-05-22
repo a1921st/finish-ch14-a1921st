c1a2ac5efc93ead0bc3becd9dfb18a09a0b13a8b
takeo.shohei <a1921st@aiit.ac.jp>
2019-05-22 16:12:42 +0000

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,6 +31,14 @@ class User < ApplicationRecord
   def feed
     Micropost.where("user_id = ?", id)
   end  
+
+  # ユーザーのステータスフィードを返す
+  def feed
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
+  end
   
   # ユーザーをフォローする
   def follow(other_user)
